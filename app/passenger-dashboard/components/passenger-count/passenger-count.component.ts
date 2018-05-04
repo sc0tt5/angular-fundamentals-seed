3ab0e23aa393d88fa6370877d38e6a7e02347675
Scott Robertson <sc0tt5@users.noreply.github.com>
2018-05-03 22:43:33 -0500

    Angular Fundamentals completed
    ...now on to Pro

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,24 @@
+import { PassengerDashboardComponent } from './../../containers/passenger-dashboard/passenger-dashboard.component';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+  selector: 'passenger-count',
+  template: `
+    <div>
+      <h3>Airline Passengers!!!</h3>
+      <div>
+        Total checked in: {{ checkedInCount() }} / {{ items?.length }}
+      </div>
+    </div>
+  `
+})
+export class PassengerCountComponent {
+  @Input() items: Passenger[];
+  checkedInCount(): number {
+    if (!this.items) return;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn)
+      .length;
+  }
+}
